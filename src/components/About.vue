bb7b6db4c822cd03a041fae244519b0738ef6269
dayoung1227 <81479945+dayoung1227@users.noreply.github.com>
2021-09-24 23:12:27 +0900

    test

--- src/components/About.vue
+++ src/components/About.vue
@@ -90,11 +90,7 @@
         <div class="card mb-3" style="max-width: 540px">
           <div class="row g-0">
             <div class="col-md-4">
-              <img
-                src="../assets/images/ldy.jpg"
-                class="img-fluid rounded-start"
-                alt="..."
-              />
+              
             </div>
             <div class="col-md-8">
               <div class="card-body">
